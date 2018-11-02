6a5173eda52d02fb6e37a4ec50b3e370bd36c638
Calvin Chiu <calvinchiu@Calvins-MacBook.local>
2018-11-02 12:26:16 -0500

    test remote

--- Main.py
+++ Main.py
@@ -16,8 +16,9 @@ class Class(arcade.Window):
     """ Main application class """
 
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Vast")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "vast")
 
+        # cool stuff
         # Set the working directory (where we expect to find files) to the same
         # directory this .py file is in. You can leave this out of your own
         # code, but it is needed to easily run the examples using "python -m"
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Vast-Game
 # vast-py
+ vast is a game made with python and the arcade lib.
